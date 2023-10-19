84a191143033d9058d1eb9a7a9b829d0d48e45fb
kalx <haikalardiansyah359@gmail.com>
2023-10-19 14:25:46 +0800

    update

---
+++ frontend/src/views/UserView.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app>
+        <Navbar />
+        <LeftBar/>
+        <User/>
+        <Footer/>
+    </v-app>
+</template>
+
+<script>
+import LeftBar from '@/components/LeftBar.vue'
+import User from '@/components/User.vue'
+import Footer from '@/components/Footer.vue'
+import Navbar from '@/components/NavBar.vue'
+export default {
+    components:{
+        LeftBar,
+        User,
+        Footer,
+        Navbar
+    }
+}
+</script>
+
+<style>
+
+</style>
