ecad3ae75be3b9b34cc16832e2d9786d923bf7b1
AnggaSaputra23 <autoruns18@gmail.com>
2023-10-19 16:15:00 +0800

    update

--- backend/app/Http/Controllers/api/AuthController.php
+++ backend/app/Http/Controllers/api/AuthController.php
@@ -32,8 +32,8 @@ class AuthController extends Controller
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
-            'data' => $user, '
-                access_token' => $token,
+            'data' => $user,
+            'access_token' => $token,
             'token_type' => 'Bearer',
         ], 201);
     }
